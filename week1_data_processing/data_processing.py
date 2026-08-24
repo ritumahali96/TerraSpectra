6b344054e19b4012bb952ef7c73f5efa0b2f01e3
Ritu Mahali <ritumahali96@gmail.com>
2026-08-24 20:35:38 +0000

    Implement band-wise normalization function

--- week1_data_processing/data_processing.py
+++ week1_data_processing/data_processing.py
@@ -10,3 +10,13 @@ gt = gt_data["salinas_gt"]
 
 print("Cube shape:", cube.shape)
 print("GT shape:", gt.shape)
+
+cube = cube.astype(np.float32)
+H, W, B = cube.shape
+cube_norm = np.zeros_like(cube)
+
+for b in range(B):
+    band = cube[:, :, b]
+    band_min = band.min()
+    band_max = band.max()
+    cube_norm[:, :, b] = (band - band_min) / (band_max - band_min + 1e-8)
