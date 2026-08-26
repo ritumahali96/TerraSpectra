991a286c76113eb3d17ad06b1fd32c81dd2d27bb
Ritu Mahali <ritumahali96@gmail.com>
2026-08-26 10:15:29 +0000

    Add PyTorch imports and GPU device check

---
+++ week2_model_building/model_training.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
