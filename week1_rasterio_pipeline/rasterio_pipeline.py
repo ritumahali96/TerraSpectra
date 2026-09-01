782f1d991a9bd16fc5a6b6db66b4b35a6881b714
Ritu Mahali <ritumahali96@gmail.com>
2026-09-01 21:04:36 +0000

    Load original Salinas .mat data

--- week1_rasterio_pipeline/rasterio_pipeline.py
+++ week1_rasterio_pipeline/rasterio_pipeline.py
@@ -2,3 +2,12 @@ import rasterio
 from rasterio.transform import from_origin
 import scipy.io as sio
 import numpy as np
+
+# Load original Salinas hyperspectral data
+loaded_data = sio.loadmat("salinas_corrected.mat")
+cube = loaded_data["salinas_corrected"]
+
+gt_data = sio.loadmat("salinas_gt.mat")
+gt = gt_data["salinas_gt"]
+
+print("Cube shape:", cube.shape)
