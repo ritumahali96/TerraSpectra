4ae1538b26bf7d3e48df4b1ff6fec3d874dfca4d
Ritu Mahali <ritumahali96@gmail.com>
2026-09-01 22:07:29 +0000

    Import Deck.gl, Mapbox, and mock farm data into FarmMap

--- week1_frontend/terraspectra-map/src/FarmMap.js
+++ week1_frontend/terraspectra-map/src/FarmMap.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import DeckGL from '@deck.gl/react';
+import { StaticMap } from 'react-map-gl';
+import { GeoJsonLayer } from '@deck.gl/layers';
+import { MAPBOX_TOKEN } from './config';
+import { farmBoundary } from './mockData';
 
 function FarmMap() {
   return <div id="map-container" style={{ width: '100%', height: '500px' }}></div>;
