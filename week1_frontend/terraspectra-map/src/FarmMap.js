af90958e3707d2d522fd5e3203d1ca6fb1b908cc
Ritu Mahali <ritumahali96@gmail.com>
2026-09-02 00:49:55 +0000

    Fix Mapbox integration using MapboxOverlay approach

--- week1_frontend/terraspectra-map/src/FarmMap.js
+++ week1_frontend/terraspectra-map/src/FarmMap.js
@@ -1,42 +1,43 @@
-import React from 'react';
-import DeckGL from '@deck.gl/react';
-import { StaticMap } from 'react-map-gl';
+import React, { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { MapboxOverlay } from '@deck.gl/mapbox';
 import { GeoJsonLayer } from '@deck.gl/layers';
 import { MAPBOX_TOKEN } from './config';
 import { farmBoundary } from './mockData';
 
+mapboxgl.accessToken = MAPBOX_TOKEN;
+
 function FarmMap() {
-  return (
-    <div style={{ width: '100%', height: '500px' }}>
-      <DeckGL
-        initialViewState={INITIAL_VIEW_STATE}
-        controller={true}
-        layers={[farmLayer]}
-      >
-        <StaticMap mapboxApiAccessToken={MAPBOX_TOKEN} />
-      </DeckGL>
-    </div>
-  );
+  const mapContainer = useRef(null);
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    if (mapRef.current) return; // prevent re-initializing
+
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/satellite-streets-v11',
+      center: [-121.65, 36.68],
+      zoom: 13
+    });
+
+    const farmLayer = new GeoJsonLayer({
+      id: 'farm-boundary',
+      data: farmBoundary,
+      filled: true,
+      getFillColor: [44, 95, 45, 100],
+      getLineColor: [151, 188, 98],
+      lineWidthMinPixels: 2
+    });
+
+    const overlay = new MapboxOverlay({ layers: [farmLayer] });
+    map.addControl(overlay);
+
+    mapRef.current = map;
+  }, []);
+
+  return <div ref={mapContainer} style={{ width: '100%', height: '500px' }} />;
 }
 
 export default FarmMap;
-
-const INITIAL_VIEW_STATE = {
-  longitude: -121.65,
-  latitude: 36.68,
-  zoom: 13,
-  pitch: 0,
-  bearing: 0
-};
-
-const farmLayer = new GeoJsonLayer({
-  id: 'farm-boundary',
-  data: farmBoundary,
-  filled: true,
-  getFillColor: [44, 95, 45, 100],
-  getLineColor: [151, 188, 98],
-  lineWidthMinPixels: 2
-});
-
-// End of Week 1 Frontend scaffolding
-// Base map + farm boundary complete; heatmap overlay comes in Week 2
