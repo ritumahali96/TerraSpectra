d92cc0482fac560e2e850fcac0719cbe8847fef9
Ritu Mahali <ritumahali96@gmail.com>
2026-09-08 04:45:14 +0000

    Write hyperspectral cube to GeoTIFF using Rasterio

--- week1_rasterio_pipeline/rasterio_pipeline.py
+++ week1_rasterio_pipeline/rasterio_pipeline.py
@@ -18,3 +18,13 @@ cube_for_tiff = np.transpose(cube, (2, 0, 1)).astype(np.float32)
 
 # Define fake geographic transform for Salinas Valley coordinates
 transform = from_origin(-121.65, 36.68, 0.0000333, 0.0000333)
+
+# Write the hyperspectral cube as a GeoTIFF file using Rasterio
+with rasterio.open(
+    "salinas_hyperspectral.tif", "w", driver="GTiff",
+    height=H, width=W, count=B, dtype=cube_for_tiff.dtype,
+    crs="EPSG:4326", transform=transform,
+) as dst:
+    dst.write(cube_for_tiff)
+
+print("GeoTIFF file created")
