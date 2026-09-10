5e3e8172d15196ae738ec510c08d269f90853400
Ritu Mahali <ritumahali96@gmail.com>
2026-09-10 06:33:59 +0000

    Remove loading text from App.js

--- week2_frontend/terraspectra-map/src/App.js
+++ week2_frontend/terraspectra-map/src/App.js
@@ -3,10 +3,7 @@ import FarmMap from './components/FarmMap';
 
 function App() {
   return (
-    <div style={{ width: '100vw', height: '100vh', background: '#f0fdf4' }}>
-      <h2 style={{ position: 'absolute', top: 10, left: 10, zIndex: 999, color: 'black' }}>
-        🚜 TerraSpectra Map Loading...
-      </h2>
+    <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0 }}>
       <FarmMap />
     </div>
   );
